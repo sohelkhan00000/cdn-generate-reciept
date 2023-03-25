5baf5f9d6538de3ee1142917784724765d51e249
Sohel khan <sohel.khan@bajajcapital.com>
2023-03-25 09:49:05 +0530

    fix error

--- assets/js/main-min.js
+++ assets/js/main-min.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-let moblNav=document.getElementById("nav-links"),linkbtn=document.getElementById("linkmblmenu"),eleBody=document.getElementsByTagName("body"),islazyLoadingDone=!1;function lazyLoading(){if(!islazyLoadingDone){islazyLoadingDone=!0;let e=document.createElement("script");e.src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js",e.async=!0,e.onload=()=>{islazyLoadingDone=!0,console.log("Script loaded successfuly")},e.onerror=()=>{islazyLoadingDone=!1,console.log("Error occurred while loading script")},document.body.appendChild(e)}}linkbtn.addEventListener("click",()=>{moblNav.classList.toggle("d-block")}),eleBody[0].addEventListener("click",()=>{lazyLoading()});
-=======
 let moblNav=document.getElementById("nav-links"),linkbtn=document.getElementById("linkmblmenu");linkbtn.addEventListener("click",()=>{moblNav.classList.toggle("d-block")});
->>>>>>> 8b0bef559b83d6e519b5c8de0105b64a5fb04d16
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,6 @@
 let moblNav = document.getElementById("nav-links");
 let linkbtn = document.getElementById("linkmblmenu");
 
-<<<<<<< HEAD
-linkbtn.addEventListener("click",()=>{
-    moblNav.classList.toggle("d-block");
-});
-
-function lazyLoading() {
-
-    if(!islazyLoadingDone)
-    {
-        islazyLoadingDone = true;
-        const script = document.createElement('script');
-        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js';
-        script.async = true;
-    
-        script.onload = () => {
-            islazyLoadingDone = true;
-            console.log('Script loaded successfuly');
-        };
-        script.onerror = () => {
-            islazyLoadingDone = false;
-            console.log('Error occurred while loading script');
-        };
-        document.body.appendChild(script);
-    }
-
-}
-
-eleBody[0].addEventListener("click", ()=>{
-    lazyLoading();
-=======
 linkbtn.addEventListener("click", () => {
     moblNav.classList.toggle("d-block");
->>>>>>> 8b0bef559b83d6e519b5c8de0105b64a5fb04d16
 });
